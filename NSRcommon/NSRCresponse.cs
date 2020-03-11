1af15840f07e4601c39fa5cceb2a5bf5030b6eb7
Evgeniy Kartavykh <evgeniy@kartavykh.com>
2020-03-11 12:19:01 +0700

    Adding a new files

---
+++ NSRcommon/NSRCresponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NSRcommon
+{
+    /// <summary>
+    /// Ответ системы
+    /// </summary>
+    [DataContract]
+    public class NSRCresponse
+    {
+        /// <summary>
+        /// Идентификатор команды
+        /// </summary>
+        [DataMember]
+        public Guid Id { get; protected set; }
+
+        /// <summary>
+        /// Признак ее выполнения
+        /// </summary>
+        [DataMember]
+        public Boolean Success { get; set; } = false;
+
+        /// <summary>
+        /// Собственно ответ
+        /// </summary>
+        [DataMember]
+        public String Response { get; set; }
+
+        public NSRCresponse(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ NeuronServerRemoteControl/Pages/ViewServer.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using NSRcommon;
+
+namespace NeuronServerRemoteControl.Pages
+{
+    public class ViewServerModel : PageModel
+    {
+        /// <summary>
+        /// сервер
+        /// </summary>
+        private NSRCserver server => NSRCservice.servers.FirstOrDefault(i => i.name.Equals(ServerName, StringComparison.InvariantCultureIgnoreCase));
+
+        /// <summary>
+        /// Кол-во отправленных команд
+        /// </summary>
+        public int SentCommandsCount => server.SentCommands.Count;
+
+        /// <summary>
+        /// Отправленные команды
+        /// </summary>
+        public IEnumerable<NSRCcommand> SentCommands => server.SentCommands.OrderByDescending(i => i.CreateDate);
+
+        /// <summary>
+        /// Имя сервера
+        /// </summary>
+        public String ServerName { get; set; }
+
+        public IActionResult OnGet(string passedObject)
+        {
+            ServerName = JsonConvert.DeserializeObject<String>(passedObject);
+
+            if (server == null)
+                return NotFound();
+
+            return Page();
+        }
+
+        public void OnPost()
+        {
+            ServerName = Request.Form["ServerName"];
+            NSRCcommand command = new NSRCcommand() { CommandText = Request.Form["CommandText"] };
+            server.AddCommand(command);
+        }
+        //public IActionResult OnGet
+    }
+}
